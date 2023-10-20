1af51a5dd01b3077122330b43205754814ba61a1
hook <3012393483@qq.com>
2023-10-20 19:14:03 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -3,22 +3,15 @@
 //! You should modify this file to make both exercises pass.
 
 fn main() {
-    // In tests7, we should set up an environment variable
-    // called `TEST_FOO`. Print in the standard output to let
-    // Cargo do it.
+    // In tests7, set up the TEST_FOO environment variable with the current timestamp.
     let timestamp = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
-        .as_secs(); // What's the use of this timestamp here?
-    let your_command = format!(
-        "Your command here with {}, please checkout exercises/tests/build.rs",
-        timestamp
-    );
+        .as_secs();
+    let your_command = format!("TEST_FOO={}", timestamp);
     println!("cargo:{}", your_command);
 
-    // In tests8, we should enable "pass" feature to make the
-    // testcase return early. Fill in the command to tell
-    // Cargo about that.
-    let your_command = "Your command here, please checkout exercises/tests/build.rs";
+    // In tests8, enable the "pass" feature.
+    let your_command = "cargo:rustc-cfg=feature=\"pass\"";
     println!("cargo:{}", your_command);
 }
